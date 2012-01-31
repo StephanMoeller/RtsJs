89b13da71f611aa69362e7d6c11c4f81abdb23e0
Stephan Ryer <stephanryer@hotmail.com>
2012-01-31 23:00:26 +0100

    made runtime adjustable latency

--- RtsJs/rtsClient.js
+++ RtsJs/rtsClient.js
@@ -17,13 +17,24 @@ rtsClient.create = function (data) {
 
     var currentNetworkTurn = 0;
     var futureExecutionStepDelay = 2; // The gap to be between next input and next output
+    var latency = 1; // Number of game turns per network turns. 1 is minimum and means 1 game turn per network turn.
+    var remainingEmptyGameTurnsToExecute = 0;
 
     // Initialize input buffer with ampty entries
     for (var i = 0; i < futureExecutionStepDelay; i++) {
         inputTurnBuffer[i] = createEmptyTurnData();
     }
 
-    var doNetworkTurn = function () {
+    var executeNextGameTurn = function () {
+        if (remainingEmptyGameTurnsToExecute > 0) {
+            // Process empty game commands
+            data.callbackTick([]);
+            remainingEmptyGameTurnsToExecute--;
+            return;
+        }
+
+        // Time to execute the next network turn (send next and process new received turn).
+        
         // Check if next input is missing
         var turnData = inputTurnBuffer[currentNetworkTurn];
         if (turnData === undefined) {
@@ -34,19 +45,33 @@ rtsClient.create = function (data) {
         // Send next output
         now.serverAddTurnData(currentNetworkTurn + futureExecutionStepDelay, outputTurnBuffer);
 
-        // Process next input
+        // Process system commands
+        for (var i = 0; i < turnData.systemCommands.length; i++) {
+            var systemCommand = turnData.systemCommands[i];
+            console.log("entry " + i);
+            console.log(systemCommand);
+            if (systemCommand.latency !== undefined) {
+                latency = systemCommand.latency;
+            }
+        }
+
+        // TODO: Notify that latency has changed
+
+        // Process next inputs game data
         data.callbackTick(turnData.gameCommands);
 
         // Count up tick number and erase output buffer
         currentNetworkTurn++;
         outputTurnBuffer = createEmptyTurnData();
-
+        remainingEmptyGameTurnsToExecute = latency - 1; // Ensures that [latency - 1] number of empty game updates will be executed before handling next network turn
+        
+        // Debug data
         updateDebugInfo();
     };
 
     now.clientStart = function () {
         data.callbackStart();
-        setInterval(doNetworkTurn, 40);
+        setInterval(executeNextGameTurn, 40);
     };
 
     // Called by the server with fixed intervals. This is the way the server synchronizes the clients.
@@ -74,6 +99,7 @@ rtsClient.create = function (data) {
                 targetGap: futureExecutionStepDelay, // The desired gap between the processed tick number and the next sent tick number from the client to the server
                 bufferSize: bufferSize,
                 lag: lag,
+                latency: latency,
                 currentNetworkTurn: currentNetworkTurn
             });
         }
@@ -84,8 +110,9 @@ rtsClient.create = function (data) {
             // Tells the server to add the specified data to the next data array sent by the server to all clients.
             outputTurnBuffer.gameCommands.push(action);
         },
-        setLatency: function(latency){
-            
+        setLatency: function (newLatency) {
+            console.log("Sendeing new latency to server: " + newLatency);
+            outputTurnBuffer.systemCommands.push({ latency: newLatency });
         }
     };
 };
